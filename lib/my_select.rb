320973835cd90701e316ba42918d13f6fbba8163
apocalypse575 <apocalypse575@gmail.com>
2017-12-21 21:35:06 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
-  collection.select { | num | num.even? }
+def my_select(array)
+  i = 0
+  select = []
+  while i < array.length
+    if yield(array[i])
+      select << array[i]
+    end
+    i+=1
+  end
+  select
 end
+
+# { | num | num.even? }
